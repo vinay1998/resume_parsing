c5d73a50fda21230f057a52cae431ae635e21d92
vinay1998 <31122301+vinay1998@users.noreply.github.com>
2017-08-28 00:12:17 +0530

    Update resume parsing.py

--- resume parsing.py
+++ resume parsing.py
@@ -17,3 +17,81 @@ class ResumeParsing:
         print("Address:"+re.search(address,parsed["content"]).group())
     if __name__=="__main__":
         parseAnyFile()
+
+        
+        
+        
+        
+ output:
+    
+					CURRICULUM  VITAE
+
+
+Paturi Anvesh
+flatno-123/3,
+nizampet,
+kukatpally,
+Hyderabad.
+Email: anvesh_98@gmail.com
+Phone No: +91-7508092027
+
+CAREER OBJECTIVE: 
+Seeking a position as a graphics designer in the field of media development at Pentagram Graphic Studio in London.
+
+EDUCATIONAL QUALIFICATIONS:
+
+Degree / Certificate                           Institution 	 			Year		CGPA/Percentage
+
+B.Tech – CSE			         Lovely Professional Univeristy,	            Pursuing 		6  – IV Semester
+				         Phagwara
+
+Intermediate	   		         NRI Academy ,			               2013	              92.2 %
+				         Hyderabad
+
+SSC				         Bhashyam Public School,		               2011		94 %
+                                                                   Hyderabad                                                                                               
+
+TECHNICAL SKILLS:
+
+Softwares     	        : MATLAB, Autocad,Microsoft Word
+Languages 	        : C, C++, Java, HTML, CSS 
+
+
+WORKSHOPS AND SEMINARS:
+ 
+Hacktrack by AIESEC, Lovely Professional University, Phagwara, April 2014
+International SWAM ROBOTICS Workshop from Technophilia Systems, Lovely Professional University, Phagwara, 
+Jan 2014
+
+EXTRA CURRICULAR ACTIVITIES: 
+
+Participated in:
+Stage play competition, Lovely Professional University, Phagwara, 2015
+IPL auction Competition, Lovely Professional University, Phagwara, 2015
+Intra School Kabadi Competition, Bhashyam public school, Hyderabad, 2009
+
+AWARDS AND HONOURS: 
+
+First Prize - University Level ROBOTICS Championship, Lovely Professional University, 2014
+Second Prize - Intra School Dance Competition, Bhashyam Public school, 2007
+First Prize – Group discussion, Prathiba Vidhya Nikethan Public School, 2005
+
+LANGUAGES KNOWN:
+
+English, Hindi, Telugu
+
+HOBBIES:
+ 
+Listening to music, Dancing , Playing badminton, watching movies
+
+
+
+------details after parsing resume--------
+Name:Paturi Anvesh
+Phone Number:7508092027
+Mail ID:anvesh_98@gmail.com
+Address:flatno-123/3,
+nizampet,
+kukatpally,
+Hyderabad.
+        
